0278e5fd42230a87e585f1344b838517dec2bf62
44mira <legolaslada@gmail.com>
2024-12-18 21:41:05 +0800

    feat: use base snippets for blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,29 +3,29 @@ return {
   version = 'v0.*',
   -- !Important! Make sure you're using the latest release of LuaSnip
   -- `main` does not work at the moment
-  dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+  dependencies = { 'rafamadriz/friendly-snippets' },
+
   opts = {
+
     keymap = {
       preset = 'default',
       ['<C-Enter>'] = { 'select_and_accept' },
       ['<C-y>'] = {}
     },
-    snippets = {
-      expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
-      active = function(filter)
-        if filter and filter.direction then
-          return require('luasnip').jumpable(filter.direction)
-        end
-        return require('luasnip').in_snippet()
-      end,
-      jump = function(direction) require('luasnip').jump(direction) end,
-    },
+
     sources = {
-      default = { 'lsp', 'path', 'luasnip', 'buffer' },
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
+
     appearance = {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = 'mono'
     },
+
+    completion = {
+      documentation = {
+        auto_show = true
+      }
+    }
   },
 }
